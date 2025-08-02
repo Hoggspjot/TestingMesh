ccb8b5fb25d0f24f2e3538b618ff2e1cf51e087e
Hoggspjot <vingisaar@gmail.com>
2025-08-02 20:46:26 +0300

    test stream

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         List<Block> blocks = List.of(
@@ -19,6 +27,37 @@ public class Test {
                 .peek(Block::squash)
                 .filter(s -> s.getParameter() < 0)
                 .forEach(System.out::println);
+
+        Stream<String> stream = Stream.of("qweq", "weqeqw")
+                .filter(s -> s.length() > 4);
+
+        Stream<Foo> stream1 = Stream.empty();
+
+        Stream<String> stream2 = Arrays.stream("gently down the stream".split(" "));
+
+
+        Path dir = Paths.get("G:\\JavaGlobal\\JavaSomePractice\\Test\\src\\main\\resources\\");
+        Stream<Path> stream3 = Files.list(dir);
+        stream3.forEach(System.out::println);
+       // Stream<Path> stream4 = Files.lines(path)
+//        List<String> list = stream3
+//                .map
+
+
+        Stream<Foo> s1 = Stream.empty();
+        Stream<Foo> s2 = Stream.empty();
+        Stream<Foo> s = Stream.concat(s1, s2);
+
+        Stream<Double> randoms = Stream.generate(Math::random);
+
+        IntStream range = IntStream.range(0, 10);
+        range.forEach(System.out::println);
+
+//        Stream.iterate(new int[]{0,1}, t -> new int[]{t[1],t[0]+t[1]})
+//                .mapToInt(t ->t[0]);
+
+
+
 
     }
 }
